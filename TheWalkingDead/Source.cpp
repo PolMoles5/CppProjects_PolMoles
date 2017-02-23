6fb90b0ab4b7efc5ba579136b98509a0770a6ce2
polmolesrivera <polmolesrivera@S3-08>
2017-02-23 13:09:23 +0100

    ?¿

--- TheWalkingDead/Source.cpp
+++ TheWalkingDead/Source.cpp
@@ -7,3 +7,45 @@ enum weapon
 	FISTS,GUN,SHOTGUN,REVOLVER,SNIPER,MACHINE_GUN,MAX
 };
 
+class Zombie;
+class Player
+{
+
+	weapon weapon=rand()%6;
+	float precision;
+	int life;
+	
+public:
+	Player();
+	void attack(Zombie &) {};
+	bool isAlive() {};
+};
+
+class Zombie {
+	int distanceToPlayer;
+	float speed;
+	float damage;
+	void attack(Player &) {};
+	bool isAlive;
+	Zombie();
+};
+
+
+
+
+
+
+
+
+
+
+
+
+int main (void)
+{
+	Player p;
+
+
+
+
+ }
